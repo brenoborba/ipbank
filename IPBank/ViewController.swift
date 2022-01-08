d98c6b0fc98cb1f75890c4d5ee258fd5695d705a
Breno Alcantara <a40541@alunos.ipb.pt>
2022-01-08 16:44:12 +0000

    DatePicker adicionado

--- IPBank/ViewController.swift
+++ IPBank/ViewController.swift
@@ -7,24 +7,15 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-    @IBOutlet weak var SearchBar: UISearchBar!
-    
-    
-
-    
-    
-    
-    
-    
-    
-    
     
 
     
